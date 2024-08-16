e7d7da2796c264545ac6d8f4df4ab79a64070a31
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-08-16 10:55:15 +0000

    Add about page

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-fron django.http import HttpResponse
+from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
